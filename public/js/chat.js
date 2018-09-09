87c27b9bf638ca00f6618df76e1c1c8d2ee3fa0e
Leandro Marino <leandrooliveiramarino@gmail.com>
2018-09-09 20:56:50 -0300

    Send messages to only people in same room

--- public/js/chat.js
+++ public/js/chat.js
@@ -30,8 +30,8 @@ socket.on('disconnect', function() {
     console.log('Disconnected from server');
 });
 
-socket.on('updateUserList', function() {
-    var ul = jQuery('<ol></ol>');
+socket.on('updateUserList', function(users) {
+    var ol = jQuery('<ol></ol>');
     users.forEach(function(user) {
         ol.append(jQuery('<li></li>').text(user));
     });
@@ -68,7 +68,6 @@ jQuery('#message-form').on('submit', function(e) {
     var messageTextbox = jQuery('[name=message]');
 
     socket.emit('createMessage', {
-        from: 'User',
         text: messageTextbox.val()
     }, function() {
         messageTextbox.val('');
