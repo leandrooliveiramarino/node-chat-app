dd22ab99055a09121b87df94e0bbde93db60fbec
Leandro Marino <leandrooliveiramarino@gmail.com>
2018-09-09 14:55:00 -0300

    Add css for chat page

--- public/js/index.js
+++ public/js/index.js
@@ -15,13 +15,46 @@ socket.on('newMessage', function(message) {
     jQuery('#messages').append(li);
 });
 
+socket.on('newLocationMessage', function(message) {
+    var li = jQuery('<li></li>');
+    var a = jQuery('<a target="_blank">My current location</a>');
+
+    li.text(`${message.from}: `);
+    a.attr('href', message.url);
+    li.append(a);
+
+    jQuery('#messages').append(li);
+});
+
 jQuery('#message-form').on('submit', function(e) {
     e.preventDefault();
 
+    var messageTextbox = jQuery('[name=message]');
+
     socket.emit('createMessage', {
         from: 'User',
-        text: jQuery('[name=message]').val()
+        text: messageTextbox.val()
     }, function() {
+        messageTextbox.val('');
+    });
+});
 
+var locationButton = jQuery('#send-location');
+locationButton.on('click', function() {
+    if(!navigator.geolocation) {
+        return alert('Geolocation not supported by your browser.');
+    }
+
+    locationButton.attr('disabled', 'disabled').text('Sending location...');
+
+    navigator.geolocation.getCurrentPosition(function(position) {
+        locationButton.removeAttr('disabled');
+        socket.emit('createLocationMessage', {
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude
+        })
+    }, function() {
+        locationButton.removeAttr('disabled').text('Send location');
+        alert('Unable to fetch location');
     });
 });
--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const http = require('http');
 const express = require('express');
 const socketIO = require('socket.io');
 
-const {generateMessage} = require('./utils/message');
+const {generateMessage, generateLocationMessage} = require('./utils/message');
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 80;
 
@@ -24,7 +24,11 @@ io.on('connection', (socket) => {
     socket.on('createMessage', (message, callback) => {
         console.log('createMessage', message);
         io.emit('newMessage', generateMessage(message.from, message.text));
-        callback('This is from the server.');
+        callback();
+    });
+
+    socket.on('createLocationMessage', (coords) => {
+        io.emit('newLocationMessage', generateLocationMessage('Admin', coords.latitude, coords.longitude))
     });
 
     socket.on('disconnect', () => {
